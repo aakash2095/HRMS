ab0054014415ea54d1484fdd9e538d76aeea4320
Aakash Soni <soniaakash5336@gmail.com>
2025-01-23 14:50:40 +0530

    update

--- hrms/department/admin.py
+++ hrms/department/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Department
+
 
 # Register your models here.
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('Dept_Name', 'status', 'created_at', 'updated_at')
+    list_filter = ('status',)
+    search_fields = ('Dept_Name',)
